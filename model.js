f58f81a627664668223893ba3b61be8f64caf2c7
dufilsfabrice1100 <batlab.official@gmail.com>
2014-09-23 20:26:57 +0200

    first 1

---
+++ model.js
@@ -0,0 +1,19 @@
+
+var mongoose  = require('mongoose');
+var crypto    = require('crypto');
+
+var Schema = mongoose.Schema; 
+var ObjectId  = Schema.ObjectId ;
+
+
+var userModel = new Shema(
+
+    {
+       name : String,  
+       vorname : String, 
+       email   : String, 
+       pass    : 
+    
+    }
+
+    );
